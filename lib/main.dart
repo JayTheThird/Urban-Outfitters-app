65194d57bd4b5eefe5a716b53d65032b1eedeed5
JayTheThird <jaypatel161200@gmail.com>
2024-04-25 16:35:40 +0530

    Added Firebase and done connectivity now working on login and sign-up

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
 // main files
-import 'package:ecommerce/widgets/support_styling_widget.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:google_fonts/google_fonts.dart';
 // project files
 import 'package:ecommerce/pages/persistent_nav_bar.dart';
+import 'package:ecommerce/widgets/support_styling_widget.dart';
 
 // created style object of SupportStylingClass
 SupportStylingClass style = SupportStylingClass();
 
 void main() async {
-  // for native splash
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  // for native splash
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  await Future.delayed(Duration(seconds: 2));
+  await Future.delayed(Duration(seconds: 1));
   FlutterNativeSplash.remove();
+
   runApp(
     const App(),
   );
@@ -26,6 +30,14 @@ final theme = ThemeData(
 
 class App extends StatelessWidget {
   const App({super.key});
+
+  // Firebase
+  Future<void> initializeDefault() async {
+    // Represents a single Firebase app instance. You can get an instance by calling [Firebase.app()]
+    FirebaseApp app = await Firebase
+        .initializeApp(); //The entry point for accessing Firebase.
+    print('Initialized default app $app');
+  }
 
   @override
   Widget build(BuildContext context) {
