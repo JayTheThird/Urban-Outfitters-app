468a6a9e48695418158dff5364660bf4c0c00361
JayTheThird <jaypatel161200@gmail.com>
2024-05-20 18:39:54 +0530

    update database

--- lib/services/database/database.dart
+++ lib/services/database/database.dart
@@ -2,10 +2,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Database {
+  // The entry point for accessing a FirebaseFirestore
+  final db = FirebaseFirestore.instance;
+
+  // add user and details
   Future addUserDetails(Map<String, dynamic> userInfo, String id) async {
-    return await FirebaseFirestore.instance
-        .collection("users")
-        .doc(id)
-        .set(userInfo);
+    return await db.collection("users").doc(id).set(userInfo);
   }
 }
