1dc84dda9b916154bd54eaaf84fdb6bcc3ecb755
JayTheThird <jaypatel161200@gmail.com>
2024-05-27 18:39:21 +0530

    added overlay functionality when admin login then it will display

--- lib/screens/admin/admin_login.dart
+++ lib/screens/admin/admin_login.dart
@@ -9,6 +9,7 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:ecommerce/main.dart';
 import 'package:ecommerce/widgets/utilities/user_textfield.dart';
 import 'package:ecommerce/widgets/utilities/buttons.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 
 class AdminLogin extends StatefulWidget {
   const AdminLogin({super.key});
@@ -25,54 +26,48 @@ class _AdminLoginState extends State<AdminLogin> {
   final userNameController = TextEditingController();
   final passwordController = TextEditingController();
 
-  bool isLoading = false;
-  bool isLoadingGoogle = false;
-  bool isLoadingFacebook = false;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Admin Login',
-                  style: TextStyle(
-                    color: style.color1,
-                    fontSize: 18,
+        child: LoaderOverlay(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Admin Login',
+                    style: TextStyle(
+                      color: style.color1,
+                      fontSize: 18,
+                    ),
                   ),
-                ),
-                SizedBox(height: 25),
-                // username textfield
-                MyTextField(
-                  controller: userNameController,
-                  keyboardType: TextInputType.name,
-                  hintText: 'User Name',
-                  obscureText: false,
-                ),
-                SizedBox(height: 12),
-                // password textfield
-                MyTextField(
-                  controller: passwordController,
-                  keyboardType: TextInputType.text,
-                  hintText: 'Password',
-                  obscureText: true,
-                ),
-                SizedBox(height: 10),
-                // log in button
-                isLoading
-                    ? SpinKitWave(
-                        color: style.color1,
-                      )
-                    : LoginSignUpButtons(
-                        onTap: _adminLogin,
-                        message: "Log in",
-                      ),
-              ],
+                  SizedBox(height: 25),
+                  // username textfield
+                  MyTextField(
+                    controller: userNameController,
+                    keyboardType: TextInputType.name,
+                    hintText: 'User Name',
+                    obscureText: false,
+                  ),
+                  SizedBox(height: 12),
+                  // password textfield
+                  MyTextField(
+                    controller: passwordController,
+                    keyboardType: TextInputType.text,
+                    hintText: 'Password',
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 10),
+                  // log in button
+                  LoginSignUpButtons(
+                    onTap: _onTapShowOverlay,
+                    message: "Log in",
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -80,40 +75,60 @@ class _AdminLoginState extends State<AdminLogin> {
     );
   }
 
-  // clear values of text editing controller
-  void clearTextEditingController() {
-    userNameController.clear();
-    passwordController.clear();
-  }
+  // overlay
+  void _onTapShowOverlay() {
+    if (userNameController.text.isEmpty) {
+      // Check if Username is empty
+      supportingWidgets.textFieldCantBeEmpty(userNameController, context, "User Name field Empty");
+      return;
+    }
 
-  // sign user in method
-  void _adminLogin() async {
-    setState(() {
-      isLoading = true;
-    });
+    if (passwordController.text.isEmpty) {
+      // Check if password is empty
+      supportingWidgets.textFieldCantBeEmpty(passwordController, context, "Password field Empty");
+      return;
+    }
 
-    final instance = FirebaseFirestore.instance;
-
-    await instance.collection("Admin").get().then(
-      (snapshot) {
+    // calling _adminLogin
+    FirebaseFirestore.instance.collection("Admin").get().then(
+      (snapshot) async {
         for (var result in snapshot.docs) {
-          if (result.data()["user-name"] != userNameController.text.trim()) {
-            supportingWidgets.alertSnackBar(context, "username invalid!");
-          } else if (result.data()["password"] != passwordController.text.trim()) {
-            supportingWidgets.alertSnackBar(context, "password invalid!");
+          if (result.data()["user-name"] != userNameController.text.trim() &&
+              result.data()["password"] != passwordController.text.trim()) {
+            supportingWidgets.alertSnackBar(context, "credential invalid!");
           } else {
+            context.loaderOverlay.show(
+              widgetBuilder: (_) {
+                return Center(
+                  child: SpinKitWave(
+                    color: style.color1,
+                  ),
+                );
+              },
+            );
+            await Future.delayed(const Duration(seconds: 2));
+            // ignore: use_build_context_synchronously
             Navigator.of(context).push(
               MaterialPageRoute(
                 builder: (ctx) => AdminHome(),
               ),
             );
+            // clear textfield data
+            clearTextEditingController();
           }
         }
       },
     );
 
-    setState(() {
-      isLoading = false;
-    });
+    // Whether this State object is currently in a tree.
+    if (!mounted) return;
+    // hide overlay
+    context.loaderOverlay.hide();
+  }
+
+  // clear values of text editing controller
+  void clearTextEditingController() {
+    userNameController.clear();
+    passwordController.clear();
   }
 }
